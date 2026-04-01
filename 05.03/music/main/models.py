66998da56c48535d7638234f76933036254781a7
Zakhar <zakhar@gmail.com>
2026-04-01 18:01:27 +0300

    добавил модельисполнителя

--- 05.03/music/main/models.py
+++ 05.03/music/main/models.py
@@ -8,9 +8,17 @@ class Janri(models.Model):
     def  __str__(self):
         return self.name_en
       
+
+class Artist(models.Model):
+    name = models.CharField(max_length=500, unique=True)
+    image= models.ImageField(upload_to='artists/', null=True)
+    def __str__(self):
+        return self.name
+          
 class Track(models.Model):
     title=models.CharField(max_length=100)
     duration=models.IntegerField()
     genres=models.ManyToManyField(Janri)
+    artist= models.ForeignKey(Artist, on_delete=models.RESTRICT,null=True)
     def __str__(self):
         return self.title
